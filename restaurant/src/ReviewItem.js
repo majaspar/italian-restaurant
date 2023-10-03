8727e26ca35dbeb55a48a726a4e50e90ea8ff2ae
majaspar <130063598+majaspar@users.noreply.github.com>
2023-10-03 12:18:09 +0100

    work on HomeScreen: Reviews Section

---
+++ restaurant/src/ReviewItem.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Star from './Star'
+
+export default function ReviewItem({ reviewer, rating, avatar, review }) {
+
+
+    return (
+        <div className="reviews__grid--item shadow-dark">
+            <div className="reviews__name-wrapper flex mb1">
+                <div>
+                    <h3 className="reviews__name">{reviewer}</h3>
+                    <div className="reviews__rating">{rating}</div>
+                </div>
+                <img className="reviews__img shadow-dark" src={avatar} alt="Reviewer's avatar" />
+            </div>
+            <p className="reviews__text">{review}</p>
+        </div>
+    )
+}
--- restaurant/src/Reviews.js
+++ restaurant/src/Reviews.js
@@ -1,58 +1,28 @@
-import placeholder from './assets/testimonial-photo.jpg'
+import ReviewItem from './ReviewItem';
+import Star from './Star'
 
+import placeholder from './assets/testimonial-photo.jpg';
+import vector1 from './assets/vector1.svg';
 
 export default function Reviews() {
+
+  const star1 = <Star />
+  const star2 = [<Star />, <Star />]
+  const star3 = [<Star />, <Star />, <Star />]
+  const star4 = [<Star />, <Star />, <Star />, <Star />]
+  const star5 = [<Star />, <Star />, <Star />, <Star />, <Star />]
   return (
     <section className="reviews margins mb7">
       <h2 className="section-title center mb5">Read Our Reviews</h2>
-
+      <img className="Reviews__vector-1" src={vector1} />
       <div className="reviews__grid grid">
 
-        <div className="reviews__grid--item shadow">
-          <div className="reviews__name-wrapper flex mb1">
-            <div>
-              <h3 className="reviews__name">Steve Rogers</h3>
-              <div className="reviews__rating">
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i></div>
-            </div>
-            <img className="reviews__img shadow-dark" src={placeholder} alt="" />
-          </div>
-          <p className="reviews__text">These guys are great! I had an amazing pizza last time I was there. </p>
-        </div>
-        <div className="reviews__grid--item">
-          <div className="reviews__name-wrapper flex mb1">
-            <div>
-              <h3 className="reviews__name">Nikki Chapman</h3>
-              <div className="reviews__rating">
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i></div>
-            </div>
-            <img className="reviews__img shadow-dark" src={placeholder} alt="" />
-          </div>
-          <p className="reviews__text">These guys are great! I had an amazing pizza last time I was there. </p>
-        </div>
-        <div className="reviews__grid--item">
-          <div className="reviews__name-wrapper flex mb1">
-            <div>
-              <h3 className="reviews__name">Millie Bennet</h3>
-              <div className="reviews__rating">
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i></div>
-            </div>
-            <img className="reviews__img shadow-dark" src={placeholder} alt="" />
-          </div>
-          <p className="reviews__text">The best carbonarra I've ever had! 5/5 stars. </p>
-        </div>
+        <ReviewItem reviewer="Nikki Chapman" rating={star4} avatar={placeholder} review="These guys are great! I had an amazing pizza last time I was there. All products top quality. It's a bit far from the city centre, though..." />
+
+        <ReviewItem reviewer="Alice Whittman" rating={star5} avatar={placeholder} review="The best carbonarra I've ever had! 5/5 stars. Honestly, guys, there's no better place for Italian cuisine than this restaurant!" />
+
+        <ReviewItem reviewer="Veronica Smith" rating={star5} avatar={placeholder} review="Amazing place! Everything I tasted was delicious. The chef made my favourite customized pizza with all the best toppings! Yum!" />
+
       </div>
 
       <h2 className="section-title center mt7 mb3">Would You Like to Leave a Review? &rarr; <a href="">Log In</a></h2>
