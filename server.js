50eb6e984b97cd025fbd5011790c34deacfb8400
majaspar <130063598+majaspar@users.noreply.github.com>
2023-10-08 00:24:11 +0100

    ready to deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dishRoute = require('./routes/dishRoute');
 const userRoute = require('./routes/userRoute');
 const ordersRoute = require('./routes/ordersRoute');
+const path = require('path')
 
 const db = require("./db.js")
 
@@ -16,15 +17,22 @@ app.use('/api/dishes/', dishRoute)               //if url is coming with 'api/di
 app.use('/api/users/', userRoute)
 app.use('/api/orders/', ordersRoute)
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use('/', express.static('restaurant/build'))
+if (process.env.NODE_ENV === 'production') {
 
-//     app.get('*', (req, res) => {
+    app.use(express.static('client/build'))
 
-//         res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+    app.get('*', (req, res) => {
 
-//     })
-// }
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+
+    })
+}
+
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "https://italianrestaurant.onrender.com/");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.get('/', (req, res) => {
     res.send("Server working🔥 on port " + port)
