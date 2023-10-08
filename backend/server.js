b367aca342b7244e9728124f5f4deb0d5ad00240
majaspar <130063598+majaspar@users.noreply.github.com>
2023-10-08 22:41:11 +0100

    deploy after moving backend into separate dir

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dishRoute = require('./routes/dishRoute');
 const userRoute = require('./routes/userRoute');
 const ordersRoute = require('./routes/ordersRoute');
 const path = require('path')
+const cors = require('cors')
 
 const db = require("./db.js")
 
@@ -12,6 +13,10 @@ const port = process.env.PORT || 5000;
 
 
 app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:3000", "https://restaurant-mqgk.onrender.com/"]
+}))
+
 
 app.use('/api/dishes/', dishRoute)               //if url is coming with 'api/dishes/' we navigate it to dishroute
 app.use('/api/users/', userRoute)
