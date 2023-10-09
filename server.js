dd097588a2be8b3c0f9f9344eac2a4e7d79f5325
majaspar <130063598+majaspar@users.noreply.github.com>
2023-10-09 14:54:52 +0100

    restructure backend

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ app.use('/api/orders/', ordersRoute)
 
 if (process.env.NODE_ENV === 'production') {
 
-    app.use(express.static('../client/build'))
+    app.use(express.static('client/build'))
 
     app.get('*', (req, res) => {
 
-        res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
 
     })
 }
